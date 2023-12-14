c75cc3a2e0746487570540692d0a900111a16c0e
liujianzhang.901 <liujianzhang.901@bytedance.com>
2023-12-14 14:47:43 +0800

    update imageknife to 797eb69
    Change-Id: I124d02fc918b72182e35a8f4221f6aa2603e2606

--- imageknife/src/main/ets/components/3rd_party/upng/UPNG.js
+++ imageknife/src/main/ets/components/3rd_party/upng/UPNG.js
@@ -20,8 +20,7 @@
 *LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 *SOFTWARE.*/
-import {deflate} from '../pako/lib/deflate'
-var pako = {deflate}
+import pako from 'pako'
 const UZIP = null;
 
 export {
