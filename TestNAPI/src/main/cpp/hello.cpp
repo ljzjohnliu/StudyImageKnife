2764b635e334c67c3a6256e512c889152672643e
liujianzhang.901 <liujianzhang.901@bytedance.com>
2024-01-04 15:13:47 +0800

    添加native接口调用
    Change-Id: If41d5f6608c3728be65fae3ddf18cb2da373be2b

--- TestNAPI/src/main/cpp/hello.cpp
+++ TestNAPI/src/main/cpp/hello.cpp
@@ -24,14 +24,53 @@ static napi_value Add(napi_env env, napi_callback_info info)
     napi_create_double(env, value0 + value1, &sum);
 
     return sum;
+}
 
+static napi_value CallNative(napi_env env, napi_callback_info info) {
+    size_t argc = 2;
+    // 声明参数数组
+    napi_value args[2] = {nullptr};
+
+    // 获取传入的参数并依次放入参数数组中
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    // 依次获取参数
+    double value0;
+    napi_get_value_double(env, args[0], &value0);
+    double value1;
+    napi_get_value_double(env, args[1], &value1);
+
+    // 返回两数相乘的结果
+    napi_value product;
+    napi_create_double(env, value0 * value1, &product);
+    return product;
+}
+
+static napi_value NativeCallArkTS(napi_env env, napi_callback_info info) {
+    size_t argc = 1;
+    // 声明参数数组
+    napi_value args[1] = {nullptr};
+
+    // 获取传入的参数并依次放入参数数组中
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    // 创建一个int，作为ArkTS的入参
+    napi_value argv = nullptr;
+    napi_create_int32(env, 8, &argv);
+
+    // 调用传入的callback，并将其结果返回
+    napi_value result = nullptr;
+    napi_call_function(env, nullptr, args[0], 1, &argv, &result);
+    return result;
 }
 
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
 {
     napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"callNative", nullptr, CallNative, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"nativeCallArkTS", nullptr, NativeCallArkTS, nullptr, nullptr, nullptr, napi_default, nullptr},
     };
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
--- TestNAPI/src/main/cpp/types/libtestnapi/index.d.ts
+++ TestNAPI/src/main/cpp/types/libtestnapi/index.d.ts
@@ -1,1 +1,3 @@
 export const add: (a: number, b: number) => number;
+export const callNative: (a: number, b: number) => number;
+export const nativeCallArkTS: (cb: (a: number) => number) => number;
